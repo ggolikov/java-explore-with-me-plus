950ed27b36d142af6fa3288ab76841d276cc6dbd
IgAlGen <genvarskiy.i.a@gmail.com>
2026-01-07 19:49:16 +0500

    Добавлено Category. Два Error и Обработчик ошибок.

--- main-service/src/main/java/ewm/category/model/Category.java
+++ main-service/src/main/java/ewm/category/model/Category.java
@@ -1,26 +1,24 @@
 package ewm.category.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(
-        name = "categories",
-        uniqueConstraints = @UniqueConstraint(name = "uq_category_name", columnNames = "name")
-)
+@Table(name = "categories")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
     private Long id;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, unique = true)
     private String name;
 }
---
+++ main-service/src/main/java/ewm/common/error/ErrorHandler.java
@@ -0,0 +1,33 @@
+package ewm.common.error;
+
+import ewm.common.dto.ApiError;
+import ewm.common.exception.ConflictException;
+import ewm.common.exception.NotFoundException;
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiError> handleNotFound(NotFoundException ex) {
+        return buildError(ex.getMessage(), "Не найден необходимый объект.", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ApiError> handleConflict(ConflictException ex) {
+        return buildError(ex.getMessage(), "Не выполнены условия для запрашиваемой операции.", HttpStatus.CONFLICT);
+    }
+
+    private ResponseEntity<ApiError> buildError(String message, String reason, HttpStatus status) {
+        ApiError error = ApiError.builder()
+                .message(message)
+                .reason(reason)
+                .status(status.name())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(error, status);
+    }
+}
