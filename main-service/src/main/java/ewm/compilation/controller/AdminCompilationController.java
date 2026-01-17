984bbce8e9ef47cdc1d130f64a539319bd1421c2
Grigory Golikov <golikov@nextbi.ru>
2026-01-17 08:57:36 +0300

    Fix linter errors

--- main-service/src/main/java/ewm/compilation/controller/AdminCompilationController.java
+++ main-service/src/main/java/ewm/compilation/controller/AdminCompilationController.java
@@ -7,7 +7,6 @@ import ewm.compilation.service.CompilationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
