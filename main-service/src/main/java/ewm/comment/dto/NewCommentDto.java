e5f3f6580ac3c2046130fe1bde26f1722acc2927
Grigory Golikov <golikov@nextbi.ru>
2026-01-17 23:53:38 +0300

    Add comments

---
+++ main-service/src/main/java/ewm/comment/dto/NewCommentDto.java
@@ -0,0 +1,14 @@
+package ewm.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class NewCommentDto {
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 5000)
+    private String text;
+}
---
+++ main-service/src/main/java/ewm/comment/dto/UpdateCommentRequest.java
@@ -0,0 +1,16 @@
+package ewm.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UpdateCommentRequest {
+    @NotBlank
+    @Size(min = 1, max = 5000)
+    private String text;
+
+    public boolean hasText() {
+        return !(text == null || text.isBlank());
+    }
+}
